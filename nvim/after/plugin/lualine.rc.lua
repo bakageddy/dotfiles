57ee66f88c30d194e16287c7b8a4c05322563d0d
Dinesh Kumar <gdineshda@gmail.com>
2022-07-26 21:31:33 +0530

    REFACTOR: lualine themes

--- nvim/after/plugin/lualine.rc.lua
+++ nvim/after/plugin/lualine.rc.lua
@@ -1,51 +1,9 @@
 local lualine = require 'lualine'
-
-local gruvbox_flat = {
-  bg       = '#1d2021',
-  fg       = '#d4be98',
-  yellow   = '#d8a657',
-  cyan     = '#89b482',
-  darkblue = '#7daea3',
-  green    = '#a9b665',
-  orange   = '#e78a4e',
-  violet   = '#d3869b',
-  magenta  = '#d3869b',
-  blue     = '#7daea3',
-  red      = '#ea6962',
-}
-
-local gruvbox = {
-  bg       = '#1d2021',
-  fg       = '#ebdbb2',
-  yellow   = '#fabd2f',
-  cyan     = '#8ec07c',
-  darkblue = '#83a598',
-  green    = '#b8bb26',
-  orange   = '#d65c0d',
-  violet   = '#d3869b',
-  magenta  = '#d3869b',
-  blue     = '#83a598',
-  red      = '#fb4934',
-}
-
-local nord = {
-  bg       = '#2E3440',
-  fg       = '#D8DEE9',
-  yellow   = '#EBCB8B',
-  cyan     = '#8FBCBB',
-  darkblue = '#81A1C1',
-  green    = '#A3B38C',
-  orange   = '#D08770',
-  violet   = '#B48EAD',
-  magenta  = '#B48EAD',
-  blue     = '#88C0D0',
-  red      = '#BF616A',
-}
-
+local themes = require 'lualine_palette'
 
 -- Color table for highlights
 -- stylua: ignore
-local colors = gruvbox
+local colors = themes.gruvbox
 
 local conditions = {
   buffer_not_empty = function()
